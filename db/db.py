5c978c7e17bd41ebfe6cd2c1a3eccf0b415b9e95
tanvisaini <71028850+tanvisaini@users.noreply.github.com>
2023-10-13 17:52:59 -0400

    Update db.py

--- db/db.py
+++ db/db.py
@@ -10,3 +10,22 @@ def fetch_pets():
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+
+
+# if we use the MTA subway gateway API, use this python script to use: 
+# import os
+
+# from underground import metadata, SubwayFeed
+
+# API_KEY = os.getenv('MTA_API_KEY')
+# ROUTE = 'Q'
+# feed = SubwayFeed.get(ROUTE, api_key=API_KEY)
+
+# # request will read from $MTA_API_KEY if a key is not provided
+# feed = SubwayFeed.get(ROUTE)
+
+# # under the hood, the Q route is mapped to a URL. This call is equivalent:
+# URL = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw'
+# feed = SubwayFeed.get(URL)
+
