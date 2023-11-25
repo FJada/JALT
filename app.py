5c7c6bf3ed0d3f6cc2305edffc85f2d0420681a7
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-11-24 22:26:51 -0500

    Add real example data for the app

--- app.py
+++ app.py
@@ -1,15 +1,58 @@
 from pymongo import MongoClient
 
-mongo_uri = "mongodb+srv://JadaF:Nipploni@cluster0.9laqhsg.mongodb.net/?retryWrites=true&w=majority"
+mongo_uri = "mongodb+srv://JadaF:Nipploni@cluster0.9laqhsg.mongodb.net/saved_addresses?retryWrites=true&w=majority"
 client = MongoClient(mongo_uri)
 
 db = client['saved_addresses']
 collection = db['collections']
 
-# Insert user data into the collection
 user_data = [
-    {"username": "user1", "addresses": [{"street": "123 Main St", "city": "Cityville", "state": "CA", "zip": "12345"}]},
-    {"username": "user2", "addresses": [{"street": "456 Business Blvd", "city": "Worktown", "state": "NY", "zip": "67890"}]},
+    {
+        "account_number": "12345",
+        "addresses": [
+            {
+                "name": "home",
+                "street": "123 Main St",
+                "city": "New York",
+                "state": "NY",
+                "zip": "10001",
+                "train_station": "Penn Station",
+                "bus_station": "Port Authority Bus Terminal"
+            },
+            {
+                "name": "work",
+                "street": "456 Business Blvd",
+                "city": "Brooklyn",
+                "state": "NY",
+                "zip": "11201",
+                "train_station": "Atlantic Terminal",
+                "bus_station": "Jay Street-MetroTech Bus Station"
+            }
+        ]
+    },
+    {
+        "account_number": "67890",
+        "addresses": [
+            {
+                "name": "home",
+                "street": "789 Residence Ln",
+                "city": "Queens",
+                "state": "NY",
+                "zip": "11354",
+                "train_station": "Flushing-Main Street Station",
+                "bus_station": "Queensboro Plaza Bus Terminal"
+            },
+            {
+                "name": "favorite bar",
+                "street": "321 Cheers St",
+                "city": "Manhattan",
+                "state": "NY",
+                "zip": "10021",
+                "train_station": "Grand Central Terminal",
+                "bus_station": "East Side Bus Terminal"
+            }
+        ]
+    },
     # Add more user data as needed
 ]
 
