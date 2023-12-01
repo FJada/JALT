88c3ca92cbd8a65470b7971eea9ae41c20768853
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-11-30 22:33:03 -0500

    Update test_addresses.py

--- data/tests/test_addresses.py
+++ data/tests/test_addresses.py
@@ -1,11 +1,16 @@
 import pytest
 
-import data.addresses as am 
+import data.addresses as am
 
 @pytest.fixture(scope='function')
 def temp_user():
     username = am._get_test_name()
-    ret = am.add_user(username, '123456', {'address': '123 Main St, New York, NY 10001', 'nearest_train_station': 'Penn Station'}, {'address': '456 Broadway, New York, NY 10002', 'nearest_train_station': 'Grand Central Terminal'})
+    ret = am.add_user(
+        username,
+        '123456',
+        {'address': '123 Main St, New York, NY 10001', 'nearest_train_station': 'Penn Station'},
+        {'address': '456 Broadway, New York, NY 10002', 'nearest_train_station': 'Grand Central Terminal'}
+    )
     yield username
     if am.user_exists(username):
         am.del_user(username)
@@ -43,7 +48,12 @@ def test_add_user_dup_name(temp_user):
     `temp_user` is the username of the user that our fixture added.
     """
     with pytest.raises(ValueError):
-        am.add_user(temp_user, '789012', {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'}, {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'})
+        am.add_user(
+            temp_user,
+            '789012',
+            {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'},
+            {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'}
+        )
 
 
 def test_add_user_blank_name():
@@ -51,7 +61,12 @@ def test_add_user_blank_name():
     Make sure a blank username raises a ValueError.
     """
     with pytest.raises(ValueError):
-        am.add_user('', '789012', {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'}, {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'})
+        am.add_user(
+            '',
+            '789012',
+            {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'},
+            {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'}
+        )
 
 
 ADD_USERNAME = 'NewUser'
@@ -59,7 +74,12 @@ ADD_USERNAME = 'NewUser'
 
 def test_add_user():
     new_username = am._get_test_name()
-    ret = am.add_user(new_username, '789012', {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'}, {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'})
+    ret = am.add_user(
+        new_username,
+        '789012',
+        {'address': '789 Broadway, New York, NY 10003', 'nearest_train_station': 'Times Square'},
+        {'address': '101 Park Ave, New York, NY 10004', 'nearest_train_station': 'Union Square'}
+    )
     assert am.user_exists(new_username)
     assert isinstance(ret, bool)
     am.del_user(new_username)
