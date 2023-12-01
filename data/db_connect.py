e7e8d4437390d50c34a9fbf3c0cb06def45d1803
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-11-30 21:31:43 -0500

    Update db_connect.py

--- data/db_connect.py
+++ data/db_connect.py
@@ -5,7 +5,7 @@ import pymongo as pm
 LOCAL = "0"
 CLOUD = "1"
 
-METRO_DB = 'metroDB'
+GAME_DB = 'gamesDB'
 
 client = None
 
@@ -24,7 +24,7 @@ def connect_db():
     if client is None:  # not connected yet!
         print("Setting client because it is None.")
         if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
-            password = os.environ.get("METRO_MONGO_PW")
+            password = os.environ.get("GAME_MONGO_PW")
             if not password:
                 raise ValueError('You must set your password '
                                  + 'to use Mongo in the cloud.')
@@ -37,7 +37,7 @@ def connect_db():
             client = pm.MongoClient()
 
 
-def insert_one(collection, doc, db=METRO_DB):
+def insert_one(collection, doc, db=GAME_DB):
     """
     Insert a single doc into collection.
     """
@@ -45,7 +45,7 @@ def insert_one(collection, doc, db=METRO_DB):
     return client[db][collection].insert_one(doc)
 
 
-def fetch_one(collection, filt, db=METRO_DB):
+def fetch_one(collection, filt, db=GAME_DB):
     """
     Find with a filter and return on the first doc found.
     """
@@ -56,21 +56,21 @@ def fetch_one(collection, filt, db=METRO_DB):
         return doc
 
 
-def del_one(collection, filt, db=METRO_DB):
+def del_one(collection, filt, db=GAME_DB):
     """
     Find with a filter and return on the first doc found.
     """
     client[db][collection].delete_one(filt)
 
 
-def fetch_all(collection, db=METRO_DB):
+def fetch_all(collection, db=GAME_DB):
     ret = []
     for doc in client[db][collection].find():
         ret.append(doc)
     return ret
 
 
-def fetch_all_as_dict(key, collection, db=METRO_DB):
+def fetch_all_as_dict(key, collection, db=GAME_DB):
     ret = {}
     for doc in client[db][collection].find():
         del doc[MONGO_ID]
