0ffc2eba82601706796be6d647b04f773635c24d
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-11-30 22:18:53 -0500

    Update db_connect.py

--- data/db_connect.py
+++ data/db_connect.py
@@ -1,43 +1,35 @@
 import os
-
 import pymongo as pm
 
 LOCAL = "0"
 CLOUD = "1"
 
-GAME_DB = 'gamesDB'
+METRO_DB = 'Metro'
 
 client = None
 
 MONGO_ID = '_id'
 
-
 def connect_db():
-    """
-    This provides a uniform way to connect to the DB across all uses.
-    Returns a mongo client object... maybe we shouldn't?
-    Also set global client variable.
-    We should probably either return a client OR set a
-    client global.
-    """
     global client
     if client is None:  # not connected yet!
         print("Setting client because it is None.")
         if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
-            password = os.environ.get("GAME_MONGO_PW")
-            if not password:
-                raise ValueError('You must set your password '
-                                 + 'to use Mongo in the cloud.')
+            username = 'af3842'
+            password = 'af3842'
+            cluster_hostname = 'cluster0.9laqhsg.mongodb.net'
+
+            if not (username and password and cluster_hostname):
+                raise ValueError('You must set your username, password, '
+                                 + 'and cluster hostname to use Mongo in the cloud.')
             print("Connecting to Mongo in the cloud.")
-            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
-                                    + '@cluster0.eqxbbqd.mongodb.net/'
-                                    + '?retryWrites=true&w=majority')
+            client = pm.MongoClient(f'mongodb+srv://{username}:{password}@{cluster_hostname}/?retryWrites=true&w=majority')
         else:
             print("Connecting to Mongo locally.")
             client = pm.MongoClient()
 
 
-def insert_one(collection, doc, db=GAME_DB):
+def insert_one(collection, doc, db=METRO_DB):
     """
     Insert a single doc into collection.
     """
@@ -45,7 +37,7 @@ def insert_one(collection, doc, db=GAME_DB):
     return client[db][collection].insert_one(doc)
 
 
-def fetch_one(collection, filt, db=GAME_DB):
+def fetch_one(collection, filt, db=METRO_DB):
     """
     Find with a filter and return on the first doc found.
     """
@@ -56,21 +48,21 @@ def fetch_one(collection, filt, db=GAME_DB):
         return doc
 
 
-def del_one(collection, filt, db=GAME_DB):
+def del_one(collection, filt, db=METRO_DB):
     """
     Find with a filter and return on the first doc found.
     """
     client[db][collection].delete_one(filt)
 
 
-def fetch_all(collection, db=GAME_DB):
+def fetch_all(collection, db=METRO_DB):
     ret = []
     for doc in client[db][collection].find():
         ret.append(doc)
     return ret
 
 
-def fetch_all_as_dict(key, collection, db=GAME_DB):
+def fetch_all_as_dict(key, collection, db=METRO_DB):
     ret = {}
     for doc in client[db][collection].find():
         del doc[MONGO_ID]
