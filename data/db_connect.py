84e86b7bdf5835cd6e5f539cb9154af575df276a
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-11-30 23:11:14 -0500

    Update db_connect.py

--- data/db_connect.py
+++ data/db_connect.py
@@ -1,5 +1,5 @@
 import os
-import pymongo as pm
+import pymongo
 
 LOCAL = "0"
 CLOUD = "1"
@@ -9,7 +9,6 @@ METRO_DB = 'Metro'
 client = None
 
 MONGO_ID = '_id'
-
 
 def connect_db():
     global client
@@ -24,48 +23,47 @@ def connect_db():
                 raise ValueError('You must set your username, password, '
                                  + 'and cluster hostname to use Mongo in the cloud.')
             print("Connecting to Mongo in the cloud.")
-            client = pm.MongoClient(f'mongodb+srv://{username}:{password}@{cluster_hostname}/?retryWrites=true&w=majority')
+            client = pymongo.MongoClient(f'mongodb+srv://{username}:{password}@{cluster_hostname}/?retryWrites=true&w=majority')
         else:
             print("Connecting to Mongo locally.")
-            client = pm.MongoClient()
+            client = pymongo.MongoClient()
 
+def get_collection(collection_name, db=METRO_DB):
+    connect_db()
+    return client[db][collection_name]
 
-def insert_one(collection, doc, db=METRO_DB):
+def insert_one(collection, doc):
     """
     Insert a single doc into collection.
     """
-    print(f'{db=}')
-    return client[db][collection].insert_one(doc)
+    print(f'{collection.db=}')
+    return collection.insert_one(doc)
 
-
-def fetch_one(collection, filt, db=METRO_DB):
+def fetch_one(collection, filt):
     """
     Find with a filter and return on the first doc found.
     """
-    for doc in client[db][collection].find(filt):
-        if MONGO_ID in doc:
-            # Convert mongo ID to a string so it works as JSON
-            doc[MONGO_ID] = str(doc[MONGO_ID])
-        return doc
+    doc = collection.find_one(filt)
+    if doc and MONGO_ID in doc:
+        # Convert mongo ID to a string so it works as JSON
+        doc[MONGO_ID] = str(doc[MONGO_ID])
+    return doc
 
-
-def del_one(collection, filt, db=METRO_DB):
+def del_one(collection, filt):
     """
     Find with a filter and return on the first doc found.
     """
-    client[db][collection].delete_one(filt)
+    collection.delete_one(filt)
 
-
-def fetch_all(collection, db=METRO_DB):
+def fetch_all(collection):
     ret = []
-    for doc in client[db][collection].find():
+    for doc in collection.find():
         ret.append(doc)
     return ret
 
-
-def fetch_all_as_dict(key, collection, db=METRO_DB):
+def fetch_all_as_dict(key, collection):
     ret = {}
-    for doc in client[db][collection].find():
+    for doc in collection.find():
         del doc[MONGO_ID]
         ret[doc[key]] = doc
     return ret
