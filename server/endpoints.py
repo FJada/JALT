855a000bed10f8d27674fbed27121a9e20f1b8e1
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-02 01:12:55 -0500

    Update endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -60,28 +60,30 @@ class AddUser(Resource):
     Adds a new user.
     """
     @api.response(HTTPStatus.CREATED, 'User Created Successfully')
+    @api.response(HTTPStatus.OK, 'Success')
     @api.response(HTTPStatus.BAD_REQUEST, 'Bad Request')
-    @api.doc(params={
-        'username': 'Username of the new user',
-        'home_address': 'Home address of the new user',
-        'work_address': 'Work address of the new user'
-    })
+    @api.route('/add_user')
+class AddUser(Resource):
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.BAD_REQUEST, 'Bad Request')
+    @api.expect(user_model)
     def post(self):
         """
         Adds a new user.
         """
         try:
-            # Extract user information from the form parameters
-            username = request.form.get('username')
-            home_address = request.form.get('home_address')
-            work_address = request.form.get('work_address')
+            # Print received parameters for debugging
+            username = request.args.get('username')
+            home_address = request.args.get('home_address')
+            work_address = request.args.get('work_address')
+            print(f"Received request: username={username}, home_address={home_address}, work_address={work_address}")
 
-            # Add the user with username, home address, and work address
-            addresses.add_user(username, account_id=None, home_address=home_address, work_address=work_address)
+            # Call the add_user function
+            addresses.add_user(username, home_address=home_address, work_address=work_address)
 
-            return {'message': 'User created successfully'}, HTTPStatus.CREATED
+            return {'message': 'User created successfully'}
         except ValueError as e:
-            raise wz.BadRequest(f'{str(e)}')
+            return {'message': str(e)}, HTTPStatus.BAD_REQUEST
 
 if __name__ == '__main__':
     app.run(debug=True)
