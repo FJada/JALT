628961b4914fe9159799547a7f590e060dcb1edc
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-01 22:55:29 -0500

    Update addresses.py

--- data/addresses.py
+++ data/addresses.py
@@ -40,17 +40,22 @@ def user_exists(username):
 
 
 # Use this function to add a user
-def add_user(username, account_id, home_address, work_address):
+def add_user(username, account_id, home_address=None,
+             work_address=None):
     if not user_exists(username):
         user_collection = dbc.get_collection(USERS_COLLECTION)
         user_doc = {
             USERNAME: username,
             ACCOUNT_ID: account_id,
-            ADDRESSES: {
-                HOME: home_address,
-                WORK: work_address
-            }
+            ADDRESSES: {}
         }
+
+        if home_address:
+            user_doc[ADDRESSES][HOME] = home_address
+
+        if work_address:
+            user_doc[ADDRESSES][WORK] = work_address
+
         dbc.insert_one(user_collection, user_doc)
 
 
@@ -59,6 +64,20 @@ def del_user(username):
     if user_exists(username):
         user_collection = dbc.get_collection(USERS_COLLECTION)
         dbc.del_one(user_collection, {USERNAME: username})
+
+
+def add_address(username, address_type, new_address):
+    user_collection = dbc.get_collection(USERS_COLLECTION)
+    user = dbc.fetch_one(user_collection, {USERNAME: username})
+
+    if user:
+        addresses = user.get(ADDRESSES, {})
+        addresses[address_type] = new_address
+
+        dbc.update_one(user_collection, {USERNAME: username},
+                       {'$set': {ADDRESSES: addresses}})
+    else:
+        raise ValueError(f'User {username} not found.')
 
 
 def main():
