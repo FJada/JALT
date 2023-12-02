1fecd5eabf207e2a4b8180b021b6092f6e06f3c8
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-01 22:44:39 -0500

    Update endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,25 +1,50 @@
-from flask import Flask, jsonify
+from http import HTTPStatus
+from flask import Flask, request
+from flask_restx import Resource, Api, fields
+import werkzeug.exceptions as wz
+from data import addresses
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/get-mta-data/<account_number>')
-def get_mta_data(account_number):
-    mta_data = {"account_number": "12345",
-        "addresses": [
-            {
-                "name": "home",
-                "street": "123 Main St",
-                "city": "New York",
-                "state": "NY",
-                "zip": "10001",
-                "train_station": "Penn Station",
-                "bus_station": "Port Authority Bus Terminal"
-            }}
-    extra = request.args.get("extra")
-    if extra:
-        mta_data["extra"] = extra
-    
-    return jsonify(mta_data)
+DELETE = 'delete'
+DEFAULT = 'Default'
+ADDRESS_MENU_EP = '/address_menu'
+ADDRESS_MENU_NM = 'Address Menu'
+ADDRESSES_EP = '/addresses'
+DEL_USER_EP = f'{ADDRESSES_EP}/{DELETE}'
+
+@api.route(f'{ADDRESSES_EP}')
+class Addresses(Resource):
+    """
+    This class supports fetching a list of all addresses.
+    """
+    def get(self):
+        """
+        This method returns all addresses.
+        """
+        return {
+            addresses.USERNAME: addresses.USERNAME,
+            addresses.ACCOUNT_ID: addresses.ACCOUNT_ID,
+            addresses.ADDRESSES: addresses.ADDRESSES,
+        }
+
+@api.route(f'{DEL_USER_EP}/<username>')
+class DelUser(Resource):
+    """
+    Deletes a user by username.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def delete(self, username):
+        """
+        Deletes a user by username.
+        """
+        try:
+            addresses.del_user(username)
+            return {username: 'Deleted'}
+        except ValueError as e:
+            raise wz.NotFound(f'{str(e)}')
 
 if __name__ == '__main__':
     app.run(debug=True)
