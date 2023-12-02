200b7735989d2667dba0a0c673d258cdd3517e38
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-02 00:24:20 -0500

    Update addresses.py

--- data/addresses.py
+++ data/addresses.py
@@ -3,6 +3,7 @@ addresses.py: the interface to our user data.
 """
 import random
 import data.db_connect as dbc
+
 USERS_COLLECTION = 'Addresses'
 ID_LEN = 24
 BIG_NUM = 100_000_000_000_000_000_000
@@ -14,19 +15,28 @@ HOME = 'home'
 WORK = 'work'
 ADDRESS = 'address'
 NEAREST_TRAIN_STATION = 'nearest_train_station'
+
+
 def _gen_id() -> str:
     _id = random.randint(0, BIG_NUM)
     _id = str(_id)
     _id = _id.rjust(ID_LEN, '0')
     return _id
+
+
 def get_users() -> dict:
     dbc.connect_db()
     return dbc.fetch_all_as_dict(USERNAME, USERS_COLLECTION)
-# Use this function to check if a user exists
+
+
+def get_all_users():
+    dbc.connect_db()
+    return dbc.fetch_all(USERS_COLLECTION)
+
+
 def user_exists(username):
     filt = {USERNAME: username}
-    return dbc.fetch_one(dbc.get_collection(USERS_COLLECTION),
-                         filt) is not None
+    return dbc.fetch_one(dbc.get_collection(USERS_COLLECTION), filt) is not None
 
 
 # Use this function to add a user
