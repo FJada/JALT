fb732bcf69eb9cf17cc084b59c235d748f509078
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-01 21:34:09 -0500

    Update alt_mta_endpoints.py

--- alt_mta_endpoints.py
+++ alt_mta_endpoints.py
@@ -1,26 +1,16 @@
-"""
-This is the file with some of the endpoints for our flask app
-The endpoint called `endpoints` will return all available endpoints.
+from http import HTTPStatus
+from flask import Flask, request
+from flask_restx import Resource, Api, fields
 
-"""
-from flask import Flask
-from flask_restx import Resource, Api
+import werkzeug.exceptions as wz
 
-app = Flask(__name__)
-api = Api(app)
-
-from flask import Flask, jsonify, request
-from flask_restx import Resource, Api
-import requests
+import data.addresses as addr
+import data.users as users
 
 app = Flask(__name__)
 api = Api(app)
 
-MTA_API_BASE_URL_ACE = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace"
-MTA_API_KEY = "KBf8CHIirk2T8svAwIqS68ZtrnJQ5pypIrLuluUh" 
-
-# Set up the request headers with the API key
-headers = {"x-api-key": MTA_API_KEY}
+# ... (unchanged imports)
 
 @api.route('/endpoints')
 class Endpoints(Resource):
@@ -34,40 +24,46 @@ class Endpoints(Resource):
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
-    
-# Endpoint to get subway line data for group
-@api.route('/subway-lines')
-class SubwayLines_ACE(Resource):
+
+# ... (unchanged classes)
+
+@api.route(f'{USERS_EP}')
+class Users(Resource):
+    """
+    This class supports fetching a list of all users.
+    """
     def get(self):
+        """
+        This method returns all users.
+        """
+        return {
+            TYPE: DATA,
+            TITLE: 'Current Users',
+            DATA: addr.get_users(),  # Update to use get_users from addresses.py
+            MENU: USER_MENU_EP,
+            RETURN: MAIN_MENU_EP,
+        }
+
+# ... (unchanged classes)
+
+@api.route(f'{DEL_USER_EP}/<username>')
+class DelUser(Resource):
+    """
+    Deletes a user by username.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def delete(self, username):
+        """
+        Deletes a user by username.
+        """
         try:
-            # Make a request to the MTA API to get subway line data
-            response = requests.get(f"{MTA_API_BASE_URL}/subway/lines", headers=headers)
+            addr.del_user(username)  # Update to use del_user from addresses.py
+            return {username: 'Deleted'}
+        except ValueError as e:
+            raise wz.NotFound(f'{str(e)}')
 
-            if response.status_code == 200:
-                subway_lines = response.json()
-                return {"subway_lines": subway_lines}
-            else:
-                return {"error": "Failed to fetch subway lines"}, response.status_code
-
-        except Exception as e:
-            return {"error": str(e)}, 500
-
-# Endpoint to get details of a specific subway line in the ACE group
-@api.route('/subway-lines/<line_id>')
-class SubwayLineDetails_ACE(Resource):
-    def get(self, line_id):
-        try:
-            # Make a request to the MTA API to get details of a specific subway line
-            response = requests.get(f"{MTA_API_BASE_URL}/subway/lines/{line_id}", headers=headers)
-
-            if response.status_code == 200:
-                subway_line = response.json()
-                return {"subway_line": subway_line}
-            else:
-                return {"error": "Failed to fetch subway line details"}, response.status_code
-
-        except Exception as e:
-            return {"error": str(e)}, 500
+# ... (unchanged classes)
 
 if __name__ == '__main__':
     app.run(debug=True)
