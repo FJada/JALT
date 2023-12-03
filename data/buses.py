55c6c79260746c4c656653337a958aa9a7acf1ad
tanvisaini <71028850+tanvisaini@users.noreply.github.com>
2023-12-03 17:30:25 -0500

    Create buses.py
    added mock data for buses and interface for buses

---
+++ data/buses.py
@@ -0,0 +1,58 @@
+import random
+import data.db_connect as dbc
+
+# Mocked data constants for 
+BUS_ROUTES = [
+    {'route_id': '1A', 'route_name': 'Downtown Express'},
+    {'route_id': '2B', 'route_name': 'Uptown Local'},
+    {'route_id': '3C', 'route_name': 'Cross-town Shuttle'}
+]
+
+BUS_SCHEDULES = {
+    'B25': {
+        'times': ['09:00 AM', '11:30 AM', '02:00 PM']
+    },
+    'B38': {
+        'times': ['10:00 AM', '12:30 PM', '03:00 PM']
+    },
+    'B45': {
+        'times': ['09:45 AM', '01:15 PM', '04:30 PM']
+    }
+}
+
+BUS_STATIONS = {
+    'B25': {
+        'latitude': random.uniform(40.6, 40.8),
+        'longitude': random.uniform(-74.1, -73.9)
+    },
+    'B38': {
+        'latitude': random.uniform(40.7, 40.9),
+        'longitude': random.uniform(-74.0, -73.8)
+    },
+    'B45': {
+        'latitude': random.uniform(40.65, 40.85),
+        'longitude': random.uniform(-74.05, -73.85)
+    }
+}
+
+def get_bus_routes():
+    return BUS_ROUTES
+
+def get_bus_schedule(route_id):
+    return BUS_SCHEDULES.get(route_id, {})
+
+def get_bus_locations(route_id):
+    return BUS_LOCATIONS.get(route_id, {})
+
+def main():
+    routes = get_bus_routes()
+    if routes:
+        route_id = routes[0]['route_id']  # Assuming the first route for demonstration
+        schedule = get_bus_schedule(route_id)
+        locations = get_bus_locations(route_id)
+        print("Bus Routes:", routes)
+        print("Schedule for Route:", schedule)
+        print("Bus Locations:", locations)
+
+if __name__ == '__main__':
+    main()
