7850e780c79a5f06f3fb23e086623c05cc6b378a
tanvisaini <71028850+tanvisaini@users.noreply.github.com>
2023-12-05 14:46:22 -0500

    Update trains.py syntax

--- data/trains.py
+++ data/trains.py
@@ -1,11 +1,11 @@
 import random
-import data.db_connect as dbc
+from data import db_connect as dbc
 
-# Mocked data constants for 
+
 TRAIN_STOPS = [
-    {'stop_id': '101', 'stop_name': 'Van Cortlandt Park - 242 St', 'stop_line':'Broadway-Seventh Avenue Local'},
-    {'stop_id': 'A49', 'stop_name': 'Nostrand Av' ,'stop_line': 'Eighth Avenue Express'},
-    {'stop_id': 'F01', 'stop_name': 'Jamaica - 179 St' ,'stop_line': 'Queens Boulevard Express/Sixth Avenue Local'}
+    {'stop_id': '101', 'stop_name': 'Van Cortlandt Park - 242 St', 'stop_line': 'Broadway-Seventh Avenue Local'},
+    {'stop_id': 'A49', 'stop_name': 'Nostrand Av', 'stop_line': 'Eighth Avenue Express'},
+    {'stop_id': 'F01', 'stop_name': 'Jamaica - 179 St', 'stop_line': 'Queens Boulevard Express/Sixth Avenue Local'}
 ]
 
 TRAIN_SCHEDULES = {
@@ -35,14 +35,18 @@ TRAIN_LOCATIONS = {
     }
 }
 
+
 def get_train_stops():
     return TRAIN_STOPS
+
 
 def get_train_schedule(stop_id):
     return TRAIN_SCHEDULES.get(stop_id, {})
 
+
 def get_train_locations(stop_id):
     return TRAIN_LOCATIONS.get(stop_id, {})
+
 
 def main():
     stops = get_train_stops()
@@ -53,6 +57,7 @@ def main():
         print("Train Stops:", stops)
         print("Schedule for Stop:", schedule)
         print("Stop Location:", locations)
+
 
 if __name__ == '__main__':
     main()
