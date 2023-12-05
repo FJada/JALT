fb6c0f949c2793802322946c25961bbf21ed0db9
tanvisaini <71028850+tanvisaini@users.noreply.github.com>
2023-12-05 13:20:49 -0500

    Update endpoints.py added endpoints from other file

--- server/endpoints.py
+++ server/endpoints.py
@@ -16,12 +16,20 @@ user_model = api.model('User', {
 })
 
 
-DELETE = 'delete'
 DEFAULT = 'Default'
-ADDRESS_MENU_EP = '/address_menu'
-ADDRESS_MENU_NM = 'Address Menu'
-ADDRESSES_EP = '/addresses'
-DEL_USER_EP = f'{ADDRESSES_EP}/{DELETE}'
+MENU = 'menu'
+MAIN_MENU_EP = '/MainMenu'
+MAIN_MENU_NM = "MTA Route Planner"
+HELLO_EP = '/hello'
+HELLO_RESP = 'hello'
+USERS_EP = '/users'
+DEL_USER_EP = f'{USERS_EP}/delete'  # Adjusted endpoint for deleting users
+USER_MENU_EP = '/user_menu'
+USER_MENU_NM = 'User Menu'
+TYPE = 'Type'
+DATA = 'Data'
+TITLE = 'Title'
+RETURN = 'Return'
 
 
 @api.route(f'{ADDRESSES_EP}')
@@ -43,6 +51,101 @@ class Addresses(Resource):
         }
 
 
+@api.route(HELLO_EP)
+class HelloWorld(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def get(self):
+        """
+        A trivial endpoint to see if the server is running.
+        It just answers with "hello world."
+        """
+        return {HELLO_RESP: 'world'}
+
+
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    def get(self):
+        """
+        The `get()` method will return a list of available endpoints.
+        """
+        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
+
+
+@api.route(f'{MAIN_MENU_EP}')
+class MainMenu(Resource):
+    """
+    This will deliver our main menu.
+    """
+    def get(self):
+        """
+        Gets the main game menu.
+        """
+        return {TITLE: MAIN_MENU_NM,
+                DEFAULT: 2,
+                'Choices': {
+                    '1': {'url': '/', 'method': 'get',
+                          'text': 'List Available Characters'},
+                    '2': {'url': '/',
+                          'method': 'get', 'text': 'List Active Users'},  # Updated text
+                    '3': {'url': f'{USERS_EP}',
+                          'method': 'get', 'text': 'List Users'},
+                    '4': {'url': '/',
+                          'method': 'get', 'text': 'Illustrating a Point!'},
+                    'X': {'text': 'Exit'},
+                }}
+
+
+@api.route(f'{USER_MENU_EP}')
+class UserMenu(Resource):
+    """
+    This will deliver our user menu.
+    """
+    def get(self):
+        """
+        Gets the user menu.
+        """
+        return {
+                   TITLE: USER_MENU_NM,
+                   DEFAULT: '0',
+                   'Choices': {
+                       '1': {
+                            'url': '/',
+                            'method': 'get',
+                            'text': 'Get User Details',
+                       },
+                       '0': {
+                            'text': 'Return',
+                       },
+                   },
+               }
+
+
+@api.route(f'{USERS_EP}')
+class Users(Resource):
+    """
+    This class supports fetching a list of all users.
+    """
+    def get(self):
+        """
+        This method returns all users.
+        """
+        return {
+            TYPE: DATA,
+            TITLE: 'Current Users',
+            DATA: addr.get_users(),
+            MENU: USER_MENU_EP,
+            RETURN: MAIN_MENU_EP,
+        }
+
+
 @api.route(f'{DEL_USER_EP}/<username>')
 class DelUser(Resource):
     """
@@ -55,10 +158,28 @@ class DelUser(Resource):
         Deletes a user by username.
         """
         try:
-            addresses.del_user(username)
+            addr.del_user(username)
             return {username: 'Deleted'}
         except ValueError as e:
             raise wz.NotFound(f'{str(e)}')
+
+
+@api.route('/bus_routes')
+class BusRoutes(Resource):
+    """
+    This class supports fetching details of all available bus routes.
+    """
+    def get(self):
+        """
+        This method returns details of all available bus routes.
+        """
+        return {
+            TYPE: DATA,
+            TITLE: 'Available Bus Routes',
+            DATA: buses.get_bus_routes(),  
+            MENU: MAIN_MENU_EP,  
+            RETURN: MAIN_MENU_EP,  
+        }
 
 
 # Add a new endpoint for adding a user with both home and work addresses
