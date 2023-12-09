9a4a212ff44f4a3b326762b38fc7c39cf304af5f
THERemyratatouille <lm4395@nyu.edu>
2023-12-08 20:12:06 -0500

    edit common.sh

--- data/common.sh
+++ data/common.sh
@@ -7,7 +7,7 @@ USER=cluster_user
 CONNECT_STR="mongodb+srv://cluster_user:cluster_pass@cluster0.9laqhsg.mongodb.net/?retryWrites=true&w=majority"
 if [ -z "$DATA_DIR" ]; then
     # DATA_DIR="/Users/lalitm/JALT/data"
-    DATA_DIR = "/home/amyf/SWE/JALT/data"
+    DATA_DIR="/home/amyf/SWE/JALT/data"
 fi
 BKUP_DIR="$DATA_DIR/bkup"
 EXP="C:/Users/amyho/OneDrive/Desktop/school/Senior_Year/SWE/mongodb-database-tools-windows-x86_64-100.9.4/bin/mongoexport.exe"
