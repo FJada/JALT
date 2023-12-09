3b3dacccd42be40677f7c7c8c5dd3b1098fda445
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-08 19:58:43 -0500

    Update restore.sh

--- data/restore.sh
+++ data/restore.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Script to backup production database to JSON files.
 
+# Script to restore data using mongoimport
+
+# Source common.sh
 . ./common.sh
 
-for collection in ${RouteCollections[@]}; do
+for collection in "${RouteCollections[@]}"; do
     echo "Restoring $collection"
-    "$IMP" --db=$DB --collection $collection --drop --file $BKUP_DIR/$collection.json
+    "$IMP" --db="$DB" --collection="$collection" --drop --file="$BKUP_DIR/$collection.json"
 done
