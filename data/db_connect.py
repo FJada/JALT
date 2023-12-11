8a87e3a3d48d8fa33c7a8fbf87ac67a5ae57c0bd
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-10 21:50:50 -0500

    Update db_connect.py

--- data/db_connect.py
+++ data/db_connect.py
@@ -19,7 +19,8 @@ def connect_db():
         print("Setting client because it is None.")
         if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
             print("Connecting to Mongo in the cloud.")
-            client = pm.MongoClient(f'mongodb+srv://cluster_user:cluster_pass@cluster0.9laqhsg.mongodb.net/?retryWrites=true&w=majority')
+            password = "cluster_pass"
+            client = pm.MongoClient(f'mongodb+srv://cluster_user:{password}@cluster0.9laqhsg.mongodb.net/?retryWrites=true&w=majority')
 
         else:
             print("Connecting to Mongo locally.")
