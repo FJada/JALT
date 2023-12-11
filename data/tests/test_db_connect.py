9d21cb85b90208a5ca78f75e743a5fff6f59f7b7
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-10 20:16:20 -0500

    Update test_db_connect.py

--- data/tests/test_db_connect.py
+++ data/tests/test_db_connect.py
@@ -1,4 +1,5 @@
 import pytest
+import logging
 
 import data.db_connect as dbc
 
@@ -7,21 +8,32 @@ TEST_COLLECT = 'test_collect'
 # can be used for field and value:
 TEST_NAME = 'test'
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope='function')
 def temp_rec():
-    dbc.connect_db()
-    dbc.insert_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
-    # yield to our test function
-    yield
-    dbc.del_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
-
+    try:
+        dbc.connect_db()
+        dbc.insert_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
+        # yield to our test function
+        yield
+    finally:
+        dbc.del_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
 
 def test_fetch_one(temp_rec):
-    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
-    assert ret is not None
-
+    try:
+        ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
+        assert ret is not None
+    except Exception as e:
+        logger.error(f"Error in test_fetch_one: {e}")
+        raise
 
 def test_fetch_one_not_there(temp_rec):
-    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
-    assert ret is None
+    try:
+        ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
+        assert ret is None
+    except Exception as e:
+        logger.error(f"Error in test_fetch_one_not_there: {e}")
+        raise
