56e225889df1a0380aabb265ea9904f9ba033e3d
Tanvi Saini <ts4175@nyu.edu>
2023-12-15 18:59:11 -0500

    buses.py

--- data/buses.py
+++ data/buses.py
@@ -2,43 +2,93 @@ import random
 import data.db_connect as dbc
 
 BUSES_COLLECTION = 'buses'
+ID_LEN = 24
+BIG_NUM = 100_000_000_000_000_000_000
 BUS_NAME = 'busName'
 STATION_NAME = 'stationName'
 BOROUGH = 'borough'
+FAVORITE = 'favorite'
+VEHICLE_ID = 'vehicleId'
 
 
-def get_bus_routes():
-    return BUS_ROUTES
+def _gen_id() -> str:
+    """
+    Generates an id per entry for mongodb
+    """
+    _id = random.randint(0, BIG_NUM)
+    _id = str(_id)
+    _id = _id.rjust(ID_LEN, '0')
+    return _id
 
 
-def add_bus_route():
-    bus_route = {}
-    bus_route[BUS_NAME] = 'name'
-    bus_route[STATION_NAME] = 'station name'
-    bus_route[BOROUGH] = 'borough'
+def gen_vehicle_id() -> str:
+    """
+    Returns vehicle12345678987654 to identify each account by unique id
+    """
+    vehicle = 'vehicle'
+    rand_part = random.randint(0, BIG_NUM)
+    return vehicle + str(rand_part)
+
+
+def get_buses_as_dict() -> dict:
     dbc.connect_db()
-    _id = dbc.insert_one(BUSES_COLLECTION, bus_route)
+    return dbc.fetch_all_as_dict(BUS_NAME, BUSES_COLLECTION)
+
+
+def get_bus_by_bus_name(bus_name: str) -> dict:
+    dbc.connect_db()
+    return dbc.fetch_one(BUSES_COLLECTION, {BUS_NAME: bus_name})
+
+
+def bus_exists(bus_name: str) -> bool:
+    return get_bus_by_bus_name(bus_name) is not None
+
+
+def favorite_bus(bus_name: str):
+    if not bus_exists(bus_name):
+        raise ValueError(f'Update failure: {bus_name} not in database.')
+    else:
+        dbc.connect_db()
+        return dbc.update_doc(BUSES_COLLECTION, {BUS_NAME: bus_name},
+                              {FAVORITE: 1})
+
+
+def remove_favorite_bus(bus_name: str):
+    if not bus_exists(bus_name):
+        raise ValueError(f'Update failure: {bus_name} not in database.')
+    else:
+        dbc.connect_db()
+        return dbc.update_doc(BUSES_COLLECTION, {BUS_NAME: bus_name},
+                              {FAVORITE: 0})
+
+
+def add_bus(bus_name: str, vehicle_id: str, favorite: bool) -> bool:
+    if bus_exists(bus_name):
+        raise ValueError(f'Duplicate bus: {bus_name=}')
+    if not bus_name:
+        raise ValueError(f'Bus name may not be blank')
+    bus ={}
+    bus[BUS_NAME] = bus_name
+    bus[VEHICLE_ID] = vehicle_id
+    bus[FAVORITE] = 0
+    dbc.connect_db()
+    _id = dbc.insert_one(BUSES_COLLECTION, bus)
     return _id is not None
 
 
-def get_bus_schedule(route_id):
-    return BUS_SCHEDULES.get(route_id, {})
+def del_bus(bus_name: str, delete_flag: bool):
+    if delete_flag:
+        if bus_exists(bus_name):
+            return dbc.del_one(BUSES_COLLECTION, {BUS_NAME: bus_name})
+        else:
+            raise ValueError(f'Delete failure: {bus_name} not in database.')
+    else:
+        raise ValueError(f'Delete skipped')
 
 
-def get_bus_stations(route_id):
-    return BUS_STATIONS.get(route_id, {})
+def get_bus_name(bus: dict):
+    return bus.get(BUS_NAME, '')
 
 
-def main():
-    routes = get_bus_routes()
-    if routes:
-        route_id = routes[0]['route_id']  # Assuming the first route for demonstration
-        schedule = get_bus_schedule(route_id)
-        stations = get_bus_stations(route_id)
-        print("Bus Routes:", routes)
-        print("Schedule for Route:", schedule)
-        print("Bus Stations:", stations)
-
-
-if __name__ == '__main__':
-    main()
+def get_favorite(bus: dict) -> bool:
+    return bool(bus.get(FAVORITE, False))
