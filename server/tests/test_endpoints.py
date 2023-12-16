5932e4a6f99e1a42f81f5436e2b36cdcdd3a8801
Lalit Mavuduri <71141050+THERemyRatatouille@users.noreply.github.com>
2023-12-16 00:08:36 -0500

    Update test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -24,9 +24,31 @@ def test_endpoints():
     print(f'{resp_json=}')
     assert ep.OPEN_ENDPOINTS in resp_json
 
+
 def test_list_routes():
     resp = TEST_CLIENT.get(ep.ROUTE_EP)
     resp_json = resp.get_json()
     assert isinstance(resp_json, dict)
 
 
+def test_add_user():
+    # Assuming you have a TEST_CLIENT for your Flask app
+    new_user_data = {'username': 'test_user', 'account_id': '123456'}
+    resp = TEST_CLIENT.post('/add_user', json=new_user_data)
+
+    assert resp.status_code == HTTPStatus.OK
+    resp_json = resp.get_json()
+    assert 'message' in resp_json
+    assert resp_json['message'] == 'User created successfully'
+
+
+def test_delete_user():
+    # Assuming you have a TEST_CLIENT for your Flask app
+    # Assuming the user 'test_user' exists
+    username_to_delete = 'test_user'
+    resp = TEST_CLIENT.delete(f'/users/delete/{username_to_delete}')
+
+    assert resp.status_code == HTTPStatus.OK
+    resp_json = resp.get_json()
+    assert username_to_delete in resp_json
+    assert resp_json[username_to_delete] == 'Deleted'
