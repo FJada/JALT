c7f306a714b7ab6ad4ffeb200dae0e19b1cb16d3
FJada <124617739+FJada@users.noreply.github.com>
2024-04-11 21:28:32 -0400

    updated form to fm

--- data/tests/test_form.py
+++ data/tests/test_form.py
@@ -1,23 +1,23 @@
 from unittest.mock import patch
-import data.form as form
+import data.form as fm
 
 
 def test_get_form():
-    form_data = form.get_form()
+    form_data = fm.get_form()
     assert isinstance(form_data, list)
     assert len(form_data) > 0
     for field in form_data:
-        assert form.FLD_NM in field
-        assert isinstance(field[form.FLD_NM], str)
-        assert len(field[form.FLD_NM]) > 0
+        assert fm.FLD_NM in field
+        assert isinstance(field[fm.FLD_NM], str)
+        assert len(field[fm.FLD_NM]) > 0
 
 
 def test_get_form_descr():
-    assert isinstance(form.get_form_descr(), dict)
+    assert isinstance(fm.get_form_descr(), dict)
 
 
 def test_get_fld_names():
-    field_names = form.get_fld_names()
+    field_names = fm.get_fld_names()
     assert isinstance(field_names, list)
     assert len(field_names) > 0
     for name in field_names:
